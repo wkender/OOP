f68d4b1ef8d31f2b01f3d8fb7447ccd6f9ccb715
wkender <87056303+wkender@users.noreply.github.com>
2023-11-27 19:37:16 +0700

    Create Exceptions.java

---
+++ lab5/Exceptions.java
@@ -0,0 +1,27 @@
+package Exceptions;
+
+import java.util.Scanner;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		int a = 0,b = 0;
+		char c = ' ';
+		Scanner in = new Scanner(System.in);
+		try {
+		System.out.print("Введите первое число:");
+		a = in.nextInt();
+		System.out.print("Введите второе число:");
+		b = in.nextInt();
+		System.out.print("Введите операнд:");
+		c = in.next().charAt(0);
+		} catch (Exception e) {
+			System.out.println("Введите нормально!!!");
+		}
+		Calculator calc = new Calculator(a,b,c);
+		float result = calc.Result();
+		System.out.printf("Полученный результат: %f", result);
+		in.close();
+	}
+
+}
